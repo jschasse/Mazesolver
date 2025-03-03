1011bd327656b8d900751ceecd577ce90becbaf0
jschasse <jaredchasse4@msn.com>
2025-03-03 11:15:40 -0500

    added functionality to draw lines in the created window

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from window import *
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(0, 0), Point(400, 330))
+    win.draw_line(line, "black")
     win.wait_for_close()
     
     
--- window.py
+++ window.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
-
+from line import *
 
 class Window():
     def __init__(self, width, height):
@@ -23,3 +23,7 @@ class Window():
             
     def close(self):
         self.running = False
+        
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+        
