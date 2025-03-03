f14b89f954a987aecded2d6965134a607124c620
jschasse <jaredchasse4@msn.com>
2025-03-03 10:50:38 -0500

    Can open a new window at specified pixel height and width. Closing the window only requires hitting the UI button

---
+++ window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("New Window")
+        self.canvas = Canvas(self.root, bg="white", height=self.height, width=self.width)
+        self.canvas.pack()
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running == True:
+            self.redraw()
+            
+    def close(self):
+        self.running = False
